1b4047ec6cd2f98a346fbd3d1985e3f2392bf1ac
khliland <khliland@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-10-28 21:25:21 +0000

    Oppdatert i henhold til repository.umb.no/R
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/umb-statistics@21 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/mixlm/R/globals.R
+++ pkg/mixlm/R/globals.R
@@ -3,5 +3,5 @@
 if (getRversion() >= "2.15.1")  
     globalVariables(c("ActiveModel","ActiveDataSet","balanced",
 	"unrestricted","cl","outmat","rss","rsq","adjr2","cp","activeModel",
-	"rmsep"))
+	"rmsep",".levels"))
 	
